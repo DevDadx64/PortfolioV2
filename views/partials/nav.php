93c0ff028d15d68a221cb64a4e346a9b1d51ebb3
DevDadx64 <DevDadx64@outlook.com>
2024-07-13 11:10:58 -0500

    More experimenting with nav

--- views/partials/nav.php
+++ views/partials/nav.php
@@ -3,10 +3,9 @@
         <h3>Miller E | Portfolio</h3>
     </div>
     <div>
-        <a href="/" class="<?= urlIs('/') ? "selected" : "none" ?>">Home</a>
-        <a href="/projects" class="<?= urlIs('/projects') ? "selected" : "none" ?>">Projects</a>
-        <a href="/about" class="<?= urlIs('/about') ? "selected" : "none" ?>">About</a>
-        <a href="/contact" class="<?= urlIs('/contact') ? "selected" : "none" ?>">Contact</a>
+        <a href="/" class="<?= urlIs('/') ? 'selected' : '' ?>">Home</a>
+        <a href="/projects" class="<?= urlIs('/projects') ? 'selected' : '' ?>">Projects</a>
+        <a href="/about" class="<?= urlIs('/about') ? 'selected' : '' ?>">About</a>
+        <a href="/contact" class="<?= urlIs('/contact') ? 'selected' : '' ?>">Contact</a>
     </div>
-    
 </nav>
